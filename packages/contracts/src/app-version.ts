e344d4d6d6df9b001315ded8940e54b5b953cc42
edrusconi <edrusconi@gmail.com>
2026-05-01 20:04:16 -0600

    Bump app version to 1.15

--- packages/contracts/src/app-version.ts
+++ packages/contracts/src/app-version.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = "SIGE_2";
-export const APP_VERSION = "1.14";
+export const APP_VERSION = "1.15";
 export const APP_VERSION_LABEL = `v${APP_VERSION}`;
 export const APP_VERSION_TEXT = `Version ${APP_VERSION}`;
